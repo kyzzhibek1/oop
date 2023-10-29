7e964c0a402c74bca36cdaaa466665c4470f41ec
jibek <120552319+kyzzhibek1@users.noreply.github.com>
2023-10-30 00:05:54 +0600

    Add files via upload

---
+++ Amount.java
@@ -0,0 +1,18 @@
+// Amount class represents a transaction amount
+class Amount {
+    private double amount;
+    private String transactionType;
+
+    public Amount(double amount, String transactionType) {
+        this.amount = amount;
+        this.transactionType = transactionType;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+}
